f0952076ee43a5524f598d967b12bff6e011fcff
zkc2023 <131240503+zkc2023@users.noreply.github.com>
2023-04-25 17:22:53 +0800

    Update index.py

--- python-learn/api/index.py
+++ python-learn/api/index.py
@@ -24,7 +24,7 @@ class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type','text/html')
         self.end_headers()
         self.wfile.write(app.encode('utf-8'))
         return
