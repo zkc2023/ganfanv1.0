a89b735a3fb38f3291110a7448eee1b0f070f23c
zkc2023 <131240503+zkc2023@users.noreply.github.com>
2023-04-25 14:41:17 +0800

    Add files via upload

---
+++ python-learn/api/index.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from random import randint
+
+app = Flask(__name__)
+canning = [
+            '南大米', '北大米', '顺溜', '牛蛙', '饺子', '陈香贵', '干锅', '麻辣烫', '肯德基', '老乡鸡', '再抽一次',
+            '陕老顺', '那个绿色的店'
+        ]
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html', canning=canning)
+
+
+@app.route('/chisha')
+def chisha():
+    num = randint(0, len(canning) - 1)
+    return render_template('index.html', canning=canning, c=canning[num])
+
+
+app.run(debug=True)
